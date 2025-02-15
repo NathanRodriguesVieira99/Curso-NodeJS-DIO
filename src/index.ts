c0ca4ed0324c56e8af64987066de4692546694c5
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-02-15 16:56:23 -0300

    feat: install and setup typescript

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { player1, player2 } from "./players/index";
+
+(async function main() {
+  console.log(
+    `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando... \n`
+  );
+})();
