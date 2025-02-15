09b5ca236cac706647b5fcc71b3b97e7aba1a098
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-02-15 16:56:59 -0300

    feat: create players , roll dice function and main function

---
+++ src/players/index.ts
@@ -0,0 +1,4 @@
+import { player1 } from "./players";
+import { player2 } from "./players";
+
+export  { player1, player2 };
---
+++ src/players/players.ts
@@ -0,0 +1,17 @@
+import { Character } from "interfaces/characters";
+
+ export const player1: Character = {
+  NOME: "Mario",
+  VELOCIDADE: 4,
+  MANOBRABILIDADE: 3,
+  PODER: 3,
+  PONTOS: 0,
+};
+
+export const player2: Character = {
+  NOME: "Luigi",
+  VELOCIDADE: 3,
+  MANOBRABILIDADE: 4,
+  PODER: 4,
+  PONTOS: 0,
+};
